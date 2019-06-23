d04f69b8da1e9bd289c3011a4eff00e45b0a8a9f
avanin <avaninmail@gmail.com>
2019-06-24 00:51:40 +0300

    github finder

---
+++ github-finder/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Navbar from "./components/layouts/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+    );
+    this.setState({
+      users: res.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { loading, users } = this.state;
+
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users loading={loading} users={users} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
